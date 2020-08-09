b9e2eb824756afaba9c13a83c1893a2a5c6731ea
sbatsch <sbatsch2@gmail.com>
2020-08-09 15:47:23 -0500

    duckifies 'c' when it sounds like an 's'

--- daffy_duckify/duckify.rb
+++ daffy_duckify/duckify.rb
@@ -6,16 +6,24 @@ loop do
   user_input.downcase!
 
   if user_input.empty? || user_input.nil?
-    puts "I thaid! Thring please! ..."
+    puts "I thaid! Thring please..."
   else 
     break
   end
 end  
 
-if user_input.include? "s"
-  user_input.gsub!(/s/, "th")  
+sentence_has_s = user_input.include? "s"
+sentence_has_ci =  user_input.include? "ci"
+sentence_has_ce = user_input.include? "ce"
+sentence_has_cy = user_input.include? "cy"
+
+if sentence_has_s || sentence_has_ci || sentence_has_ce || sentence_has_cy
+  user_input.gsub!(/s/, "th") 
+  user_input.gsub!(/ci/, "th") 
+  user_input.gsub!(/ce/, "th") 
+  user_input.gsub!(/cy/, "th") 
 else 
-  puts "There was no 's' in your string!"
+  puts "There was nothing to duckify in your sentence!"
 end 
 
 puts "If Daffy Duck was to pronounce your sentence, he would say: #{user_input}"
