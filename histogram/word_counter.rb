79f81e0d12423e9dcb406f2b9dad9d28db25a712
sbatsch <sbatsch2@gmail.com>
2020-08-10 10:27:43 -0500

    word_counter program

---
+++ histogram/word_counter.rb
@@ -0,0 +1,13 @@
+puts "Text please: "
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+
+words.each {|word| frequencies[word] += 1}
+frequencies = frequencies.sort_by {|word, count| count}
+frequencies.reverse!
+
+frequencies.each do |word, count|
+  puts "#{word} #{count}"
+end 
